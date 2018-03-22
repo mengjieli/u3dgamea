fa25416d487280e3ba2d19d710edd1937dbfbaf9
mengjieli <279037995@qq.com>
2018-03-22 22:19:54 +0800

    提交更新

---
+++ GameA/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Animator avatar;
+	int time = 0;
+
+	// Use this for initialization
+	void Start () {
+		avatar.Play ("PlayerRun");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time++;
+		if (time == 120) {
+			avatar.Play ("PlayerStand");
+			gameObject.transform.localScale = new Vector3 (-1f, 1, 1);
+		}
+		if (time == 240) {
+			avatar.Play ("PlayerRun");
+			gameObject.transform.localScale = new Vector3 (1f, 1, 1);
+		} 
+	}
+}
